cfb68c4921e0806ed138fc7977f5ab286e913d4a
Jimmy Bogard <jimmy.bogard@gmail.com>
2018-09-28 16:00:00 -0500

    Adding debug files

---
+++ src/GlimpseCore.Client/src/shell/debug/DebugReducer.ts
@@ -0,0 +1,15 @@
+import { Action } from 'redux';
+
+export function debugReducer(
+    state,
+    action: Action
+) {
+    return state;
+}
+
+export function debugSessionReducer(
+    state,
+    action: Action
+) {
+    return state;
+}
